086e845ae4ffc362eafb431d747363b9d1008589
wolfheat <mihe82@gmail.com>
2024-12-03 02:52:13 +0100

    Updated Game Devlog

--- Assets/Scripts/UI/ConfirmReplaceScreen.cs
+++ Assets/Scripts/UI/ConfirmReplaceScreen.cs
@@ -1,11 +1,14 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class ConfirmReplaceScreen : MonoBehaviour
 {
     private int ActiveIndex = -1;
+    [SerializeField] TextMeshProUGUI index_text;
     public void SetActiveIndex(int index)
     {
         ActiveIndex = index;
+        index_text.text = "Are you sure you want to replace level "+ActiveIndex+"?";
     }
 
     public void OnAcceptReplace()
