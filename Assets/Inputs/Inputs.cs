66cd9d3b66ea0580549260aa8d2ced97b895d75e
wolfheat <mihe82@gmail.com>
2025-01-12 03:49:25 +0100

    64Bit, Responsive
    * Tried to make build work for 64Bit newer androids
    		* Tried to make UI more responsive to different resolutions

--- Assets/Inputs/Inputs.cs
+++ Assets/Inputs/Inputs.cs
@@ -2,7 +2,6 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using static UnityEditor.PlayerSettings;
 
 public class Inputs : MonoBehaviour
 {
--- Assets/Scripts/UI/NormalPanelController.cs
+++ Assets/Scripts/UI/NormalPanelController.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,30 +7,91 @@ public class NormalPanelController : MonoBehaviour
 {
     [SerializeField] Slider slider;
     [SerializeField] TextMeshProUGUI boardSizeText;
-    private bool expanded = false;
+    private bool expanded = true;
     private float heigth;
+    private Vector3 panelOpenPosition;
+    private Vector3 panelClosedPosition;
+    Coroutine activeRoutine;
+    Coroutine autoClose;
+
+    const float TotTime = 0.1f;
+    const float WaitTime = 4f;
+    const float WaitTimeFast = 0.3f;
 
     private void OnEnable()
     {
         // Initiate the Create With start of creat buttons and 
         SetSliderSize();
-        heigth = transform.GetComponent<RectTransform>().rect.height;
+    
+    }
+    private void Start()
+    {
+        heigth = transform.GetComponent<RectTransform>().rect.height;            
+        panelOpenPosition = transform.localPosition;
+        panelClosedPosition = transform.localPosition + Vector3.up*heigth;
     }
 
     public void ExpandMenu()
     {
+        if (activeRoutine != null) 
+            return;
         Debug.Log("Expand");
         if (expanded)
         {
-            transform.localPosition = new Vector3(transform.localPosition.x,transform.localPosition.y - heigth,transform.localPosition.z);
+            //transform.localPosition = panelClosedPosition;
+            activeRoutine = StartCoroutine(CloseMenu("close"));
+         //   transform.localPosition = new Vector3(transform.localPosition.x,transform.localPosition.y - heigth,transform.localPosition.z);
         }
         else
         {
-            transform.localPosition = new Vector3(transform.localPosition.x,transform.localPosition.y + heigth,transform.localPosition.z);
+            //transform.localPosition = panelOpenPosition;
+            activeRoutine = StartCoroutine(CloseMenu("open"));
+            //transform.localPosition = new Vector3(transform.localPosition.x,transform.localPosition.y + heigth,transform.localPosition.z);
         }
         expanded = !expanded;
     }
 
+    private IEnumerator AutoClose(bool fast = false)
+    {
+        float t = 0f;
+        while (t < (fast? WaitTimeFast:WaitTime))
+        {
+            t += Time.deltaTime;               
+            yield return null;
+        }
+        if(activeRoutine == null)
+            StartCoroutine(CloseMenu("close"));
+    }
+    private IEnumerator CloseMenu(string type)
+    {
+        Vector3 start = type=="close"?panelOpenPosition:panelClosedPosition;
+        Vector3 end = type == "close"?panelClosedPosition:panelOpenPosition;
+
+        float YPos = transform.localPosition.y;
+        float t = 0f;
+        while(type=="close"?(YPos < end.y): (YPos > end.y))
+        {
+            YPos = Mathf.Lerp(start.y, end.y, t/TotTime);
+            transform.localPosition = new Vector3(transform.localPosition.x,YPos, transform.localPosition.z);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        transform.localPosition = end;  
+        if(type == "open")
+        {
+            StartAutoCloseCO();            
+        }
+        else
+            expanded = false;
+        activeRoutine = null;
+    }
+
+    public void StartAutoCloseCO(bool fast = false)
+    {
+        if (autoClose != null)
+            StopCoroutine(autoClose);
+        autoClose = StartCoroutine(AutoClose(fast));
+    }
 
     public void ConfirmSettings()
     {
@@ -43,18 +105,21 @@ public class NormalPanelController : MonoBehaviour
         Debug.Log("Beginner");
         USerInfo.Instance.BoardType = BoardTypes.Beginner;
         LevelCreator.Instance.RestartGame(false,true);
+        StartAutoCloseCO(true);
     }
     public void RequestStartIntermediate()
     {
         Debug.Log("Intermediate");
         USerInfo.Instance.BoardType = BoardTypes.Intermediate;
         LevelCreator.Instance.RestartGame(false, true);
+        StartAutoCloseCO(true);
     }
     public void RequestStartExpert()
     {
         Debug.Log("Expert");
         USerInfo.Instance.BoardType = BoardTypes.Expert;
         LevelCreator.Instance.RestartGame(false, true);
+        StartAutoCloseCO(true);
     }
 
     public void UpdateCreateLevelSize()
@@ -63,6 +128,7 @@ public class NormalPanelController : MonoBehaviour
         int nexValue = (int)slider.value;
         boardSizeText.text = nexValue + "x" + nexValue;
         ConfirmSettings();
+        StartAutoCloseCO();
     }
     public void SetSliderSize() => slider.value = USerInfo.Instance.BoardSize;
 }
